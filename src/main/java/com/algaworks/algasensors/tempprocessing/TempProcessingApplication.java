e666011323b1497f1e32df7337ec4fe87557ba91
rfcarmo <rfortunatoc@gmail.com>
2025-08-12 17:02:37 +0100

    New project files.

---
+++ src/main/java/com/algaworks/algasensors/tempprocessing/TempProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.tempprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempProcessingApplication.class, args);
+	}
+
+}
