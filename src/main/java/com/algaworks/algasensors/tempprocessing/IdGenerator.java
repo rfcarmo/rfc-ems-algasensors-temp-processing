ff49b6dacf6578234b4b17a666914ee6d57202fc
rfcarmo <rfortunatoc@gmail.com>
2025-08-13 14:41:42 +0100

    Teste de identificadores.

---
+++ src/main/java/com/algaworks/algasensors/tempprocessing/IdGenerator.java
@@ -0,0 +1,18 @@
+package com.algaworks.algasensors.tempprocessing;
+
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedEpochGenerator;
+
+import java.util.UUID;
+
+public class IdGenerator {
+
+    private static final TimeBasedEpochGenerator timeBasedEpochGenerator = Generators.timeBasedEpochGenerator();
+
+    private IdGenerator() {
+    }
+
+    public static UUID generateTimeBasedUUID() {
+        return timeBasedEpochGenerator.generate();
+    }
+}
---
+++ src/test/java/com/algaworks/algasensors/tempprocessing/UUIDv7Test.java
@@ -0,0 +1,36 @@
+package com.algaworks.algasensors.tempprocessing;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+public class UUIDv7Test {
+
+    @Test
+    void shouldGenerateUUIDv7() {
+        UUID uuid1 = IdGenerator.generateTimeBasedUUID();
+        UUID uuid2 = IdGenerator.generateTimeBasedUUID();
+        UUID uuid3 = IdGenerator.generateTimeBasedUUID();
+        UUID uuid4 = IdGenerator.generateTimeBasedUUID();
+
+        System.out.println(uuid1);
+        System.out.println(uuid2);
+        System.out.println(uuid3);
+        System.out.println(uuid4);
+
+        System.out.println(UUIDv7Utils.extractOffsetDateTime(uuid1));
+        System.out.println(UUIDv7Utils.extractOffsetDateTime(uuid2));
+        System.out.println(UUIDv7Utils.extractOffsetDateTime(uuid3));
+        System.out.println(UUIDv7Utils.extractOffsetDateTime(uuid4));
+
+        UUID uuid5 = IdGenerator.generateTimeBasedUUID();
+        OffsetDateTime uuidDateTime = UUIDv7Utils.extractOffsetDateTime(uuid5).truncatedTo(ChronoUnit.MINUTES);
+        OffsetDateTime currentDateTime = OffsetDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+
+        Assertions.assertThat(uuidDateTime).isEqualTo(currentDateTime);
+    }
+
+}
